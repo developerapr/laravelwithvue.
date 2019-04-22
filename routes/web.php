9e730c5d77a7a3efa9e2d3802a4a7466c27aa6b2
Arunendra <dev.aprai@gmail.com>
2019-04-22 18:21:29 +0530

    added dynamic vue route for profile and dashboard

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+#
+Route::get('/{any}', 'HomeController@index')->where('path','([A-z\d-\/_.]+)?');
